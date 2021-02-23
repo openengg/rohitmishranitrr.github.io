df9d95e143b2983fa506afd6b89d0623c5c1ff90
rohitmishranitrr <rohitmishranitrr@gmail.com>
2021-02-23 19:20:50 +0000

    Deploying to gh-pages-mac from  @ 58067c81f2c59685ce81bb04de26e597197d1d1a 🚀

--- precache-manifest.2699e743014318555b69d4526e77f55f.js
+++ precache-manifest.2699e743014318555b69d4526e77f55f.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cdac3db0467d9d2d82ea14a991898eed",
+    "revision": "b5c6be513f6f623fc9c534e1d12a0693",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/4.887a884c.chunk.css"
   },
   {
-    "revision": "cde6fde2fdce1af40f2c",
+    "revision": "9997aa274afa28baa38b",
     "url": "/static/css/main.d4c6739e.chunk.css"
   },
   {
@@ -48,8 +48,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/9.599d6207.chunk.js"
   },
   {
-    "revision": "cde6fde2fdce1af40f2c",
-    "url": "/static/js/main.152c7ab0.chunk.js"
+    "revision": "9997aa274afa28baa38b",
+    "url": "/static/js/main.aadb5ad4.chunk.js"
   },
   {
     "revision": "3cd97f6fc28b9318ba38",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7cfcd3c013934d4e34e87645e890ba7a.js"
+  "/precache-manifest.2699e743014318555b69d4526e77f55f.js"
 );
 
 self.addEventListener('message', (event) => {
