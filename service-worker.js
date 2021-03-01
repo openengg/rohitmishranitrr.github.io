f025cc4e2338deb27f45286021c88532e556daf3
rohitmishranitrr <rohitmishranitrr@gmail.com>
2021-03-01 23:43:27 +0000

    Deploying to gh-pages from  @ d8fa322cfd045183e31316116fddce04ccca93f5 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d8204f2a26036d6a51d66647b6c874c1.js"
+  "/precache-manifest.d9c216da94fd014ae886d1ecf87d902b.js"
 );
 
 self.addEventListener('message', (event) => {
