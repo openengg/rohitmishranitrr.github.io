2753e480a68d505151d667e8d7ff75da39482da8
openengg1 <rohitmishranitrr@gmail.com>
2022-07-10 22:43:57 +0000

    Deploying to gh-pages-mac from  @ ff168bf18c7f57c599d8299311e3c607886df8ec 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a90881253cbddfb50a79749f67a22f9a.js"
+  "/precache-manifest.ab9c6ed4ab7f9ce6c30a864672589137.js"
 );
 
 self.addEventListener('message', (event) => {
